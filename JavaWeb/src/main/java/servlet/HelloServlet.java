ba66604506fffc2771fc05fd347a544422d49f65
FP <FP@FP>
2026-07-09 19:33:13 +0800

    web

--- JavaWeb/src/main/java/servlet/HelloServlet.java
+++ JavaWeb/src/main/java/servlet/HelloServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.time.LocalDateTime;
 
 import jakarta.servlet.ServletException;
@@ -14,11 +15,18 @@ public class HelloServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 設定請求編碼
+		req.setCharacterEncoding("UTF-8");
+		// 設定回應編碼
+		resp.setCharacterEncoding("UTF-8");
+		// 設定瀏覽器編碼
+		resp.setContentType("text/html;charset=UTF-8");
+		
 		// 取得現在時刻
 		LocalDateTime dt = LocalDateTime.now();
 		
 		// 建立 html 字串
-		String html = "now => " + dt.getHour() + ":" + dt.getMinute() + ":" + dt.getSecond();
+		String html = "現在時刻 => " + dt.getHour() + ":" + dt.getMinute() + ":" + dt.getSecond();
 		
 		// 回應
 		resp.getWriter().print(html); 
