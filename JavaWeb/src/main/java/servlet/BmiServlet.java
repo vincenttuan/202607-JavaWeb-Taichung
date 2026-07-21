0bd4e20c7fca73bbd0fac15cee16b232536a4f4c
FP <FP@FP>
2026-07-21 21:18:06 +0800

    web

--- JavaWeb/src/main/java/servlet/BmiServlet.java
+++ JavaWeb/src/main/java/servlet/BmiServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -28,12 +29,25 @@ public class BmiServlet extends HttpServlet {
 		// 判斷 bmi 正常, 過重, 過輕 
 		String result = (bmi > 23) ? "過重" : (bmi <= 18) ? "過輕" : "正常";
 		
+		/*
 		// html
 		String html = "身高: %.1f 體重: %.1f BMI: %.2f 判斷: %s".formatted(h, w, bmi, result);
 		
 		// 回應
 		resp.getWriter().print(html);
+		*/
 		
+		// 1.準備要給 jsp 的參數資料
+		req.setAttribute("h", h);
+		req.setAttribute("w", w);
+		req.setAttribute("bmi", bmi);
+		req.setAttribute("result", result);
+		
+		// 2.建立一個"請求分派器"用來告知要傳到哪一個 jsp 檔案
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/bmi_result.jsp");
+		
+		// 3.forward 傳送
+		rd.forward(req, resp);
 	}
 	
 }
