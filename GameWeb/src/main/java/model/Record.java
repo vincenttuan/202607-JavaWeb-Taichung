9397bb4a76687aee95b4bcaca15bf87d026c2070
FP <FP@FP>
2026-07-23 19:52:10 +0800

    web

--- GameWeb/src/main/java/model/Record.java
+++ GameWeb/src/main/java/model/Record.java
@@ -1,5 +1,33 @@
 package model;
 
+/**
+ * 每完成一場拳賽，就建立一個 Record 物件
+ * */
 public class Record {
+	
+	private int player; // 玩家所出的拳
+	private int server; // 電腦所出的拳
+	private String result; // 拳賽結果
+	
+	// 建構子
+	public Record(int player, int server, String result) {
+		super();
+		this.player = player;
+		this.server = server;
+		this.result = result;
+	}
+	
+	// getters
+	public int getPlayer() {
+		return player;
+	}
 
+	public int getServer() {
+		return server;
+	}
+
+	public String getResult() {
+		return result;
+	}
+	
 }
