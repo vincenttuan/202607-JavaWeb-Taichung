691fe7506aea479023dd3d420fa47551b54387e5
FP <FP@FP>
2026-07-28 19:19:35 +0800

    web

--- GameWeb/src/test/java/testing/TestGameService.java
+++ GameWeb/src/test/java/testing/TestGameService.java
@@ -1,5 +1,26 @@
 package testing;
 
-public class TestGameService {
+import org.junit.jupiter.api.Test;
 
+import model.Game;
+import model.Record;
+import service.GameService;
+
+public class TestGameService {
+	
+	@Test
+	public void test() {
+		String username = "andrew";
+		int userplay = 1; // 0 = 石, 1 = 布, 2 = 刀
+		
+		GameService service = new GameService();
+		Record record = service.play(username, userplay);
+		// 印出結果
+		System.out.printf("玩家 %s 出: %s%n", username, Game.getName(record.getPlayer()));
+		System.out.printf("電腦出: %s%n", Game.getName(record.getServer()));
+		System.out.printf("結果: %s%n", record.getResult());
+		
+	}
+	
+	
 }
