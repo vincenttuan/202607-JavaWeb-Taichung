5990c2553ab815cc545b9c000cc74b1a9541d66a
FP <FP@FP>
2026-08-11 21:09:37 +0800

    web

--- MemberWeb/src/main/java/controller/LoginController.java
+++ MemberWeb/src/main/java/controller/LoginController.java
@@ -1,5 +1,25 @@
 package controller;
 
-public class LoginController {
+import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
+		//rd.forward(req, resp);
+		
+		req.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(req, resp);
+		
+	}
+	
 }
