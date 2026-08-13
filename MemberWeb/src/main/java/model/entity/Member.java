b3499368aefba7b76f161c434f384b2d2e01f1ee
FP <FP@FP>
2026-08-13 20:40:06 +0800

    web

--- MemberWeb/src/main/java/model/entity/Member.java
+++ MemberWeb/src/main/java/model/entity/Member.java
@@ -4,6 +4,24 @@ import java.util.Date;
 
 import lombok.Data;
 
+/**
+ * -- 建立資料庫 --
+	create database if not exists shopping
+	default character set utf8mb4
+	collate utf8mb4_unicode_ci;
+	
+   -- 建立資料表 --	
+	create table if not exists member (
+		id int primary key auto_increment,
+    	username varchar(50) unique not null,
+    	password varchar(500) not null,
+    	email varchar(100) not null,
+    	role varchar(50) not null,
+    	create_time datetime default current_timestamp
+	);
+	
+ * */
+
 @Data
 public class Member {
 	
