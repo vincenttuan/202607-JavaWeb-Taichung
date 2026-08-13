d89432e50b4f6317670385a5a477ecc5cd5aa2b8
FP <FP@FP>
2026-08-13 19:59:36 +0800

    web

--- MemberWeb/src/main/java/controller/RegisterController.java
+++ MemberWeb/src/main/java/controller/RegisterController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.util.SHA256Util;
 
 @WebServlet("/register")
 public class RegisterController extends HttpServlet {
@@ -16,6 +17,35 @@ public class RegisterController extends HttpServlet {
 		
 		req.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(req, resp);
 		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 編碼
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+		
+		// 接收資料
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		String email = req.getParameter("email");
+		String role = req.getParameter("role");
+		
+		// Hash 與 salt
+		String salt = SHA256Util.generateSalt();
+		String hash = SHA256Util.hash(password, salt);
+		
+		String html = """
+				username = %s<p />
+				password = %s<p />
+				email = %s<p />
+				role = %s<p />
+				salt = %s<p />
+				hash = %s<p />
+				""".formatted(username, password, email, role, salt, hash);
+		
+		resp.getWriter().print(html);
 	}
 	
 }
