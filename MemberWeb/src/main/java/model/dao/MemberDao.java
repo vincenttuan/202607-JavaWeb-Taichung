1f1cb85ec3956b55734d02266958eb6341c2af40
FP <FP@FP>
2026-08-13 21:00:06 +0800

    web

--- MemberWeb/src/main/java/model/dao/MemberDao.java
+++ MemberWeb/src/main/java/model/dao/MemberDao.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import model.util.DBUtil;
@@ -16,10 +17,21 @@ public class MemberDao {
 				values(?, ?, ?, ?, ?);
 				""";
 		
-		try(Connection conn = DBUtil.) {
+		try(Connection conn = DBUtil.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.setString(1, username);
+			pstmt.setString(2, email);
+			pstmt.setString(3, role);
+			pstmt.setString(4, salt);
+			pstmt.setString(5, hash);
+			
+			// 更新
+			pstmt.executeUpdate();
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
+			throw new RuntimeException("新增失敗: " + e.getMessage());
 		}
 		
 		
