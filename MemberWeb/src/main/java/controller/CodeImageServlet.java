a3ed97ba503b9c3d2b143475291246b3a65ab99b
FP <FP@FP>
2026-08-18 19:58:24 +0800

    web

--- MemberWeb/src/main/java/controller/CodeImageServlet.java
+++ MemberWeb/src/main/java/controller/CodeImageServlet.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.security.SecureRandom;
@@ -30,6 +33,29 @@ public class CodeImageServlet extends HttpServlet {
 		// 3.開始繪圖
 		// 3.1 建立圖檔暫存區
 		BufferedImage img = new BufferedImage(80, 30, BufferedImage.TYPE_INT_RGB);
+		// 3.2 建立畫布
+		Graphics g = img.getGraphics();
+		// 3.3 設定顏色
+		g.setColor(Color.YELLOW);
+		// 3.4 塗滿背景
+		g.fillRect(0, 0, 80, 30);
+		// 3.5 設定顏色
+		g.setColor(Color.BLACK);
+		// 3.6 設定字型
+		g.setFont(new Font("Arial", Font.BOLD, 25));
+		// 3.7 繪文字
+		g.drawString(code, 10, 23);
+		// 3.8 加上干擾線
+		g.setColor(Color.RED);
+		for(int i=0;i<10;i++) {
+			int x1 = random.nextInt(80); // 0~79
+			int y1 = random.nextInt(30); // 0~29
+			int x2 = random.nextInt(80); // 0~79
+			int y2 = random.nextInt(30); // 0~29
+			
+			g.drawLine(x1, y1, x2, y2);
+		}
+		
 		
 		// 顯示圖檔, 將資料以串流格式回傳給瀏覽器
 		ImageIO.write(img, "PNG", resp.getOutputStream());
