912bc77976a99814c25f0b12728e6ec408c2d5b5
FP <FP@FP>
2026-08-18 21:26:02 +0800

    web

--- MemberWeb/src/main/java/controller/LogoutController.java
+++ MemberWeb/src/main/java/controller/LogoutController.java
@@ -15,7 +15,7 @@ public class LogoutController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession();
-		session.invalidate();
+		session.invalidate(); // session 失效
 		
 		req.setAttribute("title", "登出");
 		req.setAttribute("message", "登出成功");
