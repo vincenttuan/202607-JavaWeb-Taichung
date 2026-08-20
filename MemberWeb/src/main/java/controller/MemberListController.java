efcd57f238466019ae45fbe2d85c5a543f4a3277
FP <FP@FP>
2026-08-20 21:17:28 +0800

    web

--- MemberWeb/src/main/java/controller/MemberListController.java
+++ MemberWeb/src/main/java/controller/MemberListController.java
@@ -7,7 +7,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import model.dao.MemberDao;
+import model.entity.Member;
 
 /**
  * 會員資料列表
@@ -20,9 +22,28 @@ public class MemberListController extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 判斷是否有登入
+		HttpSession session = req.getSession();
+		// 注意, 登入成功一定會有 member 的 session 物件
+		if(session.getAttribute("member") == null) {
+			// 重導到登入頁面
+			resp.sendRedirect("/MemberWeb/login");
+			return;
+		}
 		
+		// 判斷 role=ADMIN ?
+		Member member = (Member)session.getAttribute("member");
+		if(!member.getRole().equals("ADMIN")) {
+			// 重導到結果頁
+			String title = "權限";
+			String message = "權限不足";
+			req.setAttribute("title", title);
+			req.setAttribute("message", message);
+			req.getRequestDispatcher("/WEB-INF/view/result.jsp").forward(req, resp);
+			return;
+		}
 		
-		
+		resp.getWriter().print("pass");
 	}
 	
 }
