56b628e335d115a7de314eab8a932f738a3b8278
FP <FP@FP>
2026-08-25 21:25:10 +0800

    web

--- MemberWeb/src/main/java/controller/ProfileController.java
+++ MemberWeb/src/main/java/controller/ProfileController.java
@@ -24,13 +24,7 @@ public class ProfileController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 判斷是否有登入
-		HttpSession session = req.getSession();
-		// 注意, 登入成功一定會有 member 的 session 物件
-		if(session.getAttribute("member") == null) {
-			// 重導到登入頁面
-			resp.sendRedirect("/MemberWeb/login");
-			return;
-		}
+		// 由 LoginFilter 來判斷
 		
 		// 重導到個人資料頁 profile.jsp
 		req.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(req, resp);
