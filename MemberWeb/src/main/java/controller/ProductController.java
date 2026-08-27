f9c6696257d94724759a7c09cffe745e9e88b4f8
FP <FP@FP>
2026-08-27 19:32:39 +0800

    web

--- MemberWeb/src/main/java/controller/ProductController.java
+++ MemberWeb/src/main/java/controller/ProductController.java
@@ -9,6 +9,23 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/**
+ * 商品 Controller
+ * 
+ * GET:
+ * 給予 UI 畫面
+ * 新增畫面 /products?action=new
+ * 修改畫面 /products?action=edit
+ * 列表畫面 /products?action=list 或 /products
+ * 
+ * POST:
+ * 修改資料
+ * 新增動作 /products?action=insert
+ * 修改動作 /products?action=update
+ * 刪除動作 /products?action=delete
+ * 
+ * */
+
 @WebServlet("/products")
 public class ProductController extends HttpServlet {
 	
