fa052a25d526d74b3823c7168444cdfc88a32a6c
FP <FP@FP>
2026-09-01 21:17:25 +0800

    web

--- MemberWeb/src/main/java/service/ProductService.java
+++ MemberWeb/src/main/java/service/ProductService.java
@@ -36,6 +36,21 @@ public class ProductService {
 		return product;
 	}
 	
+	// 轉 DTO
+	private ProductDto toDto(Product product) {
+		ProductDto productDto = new ProductDto();
+		
+		productDto.setId(product.getId());
+		productDto.setName(product.getName());
+		productDto.setCategory(product.getCategory());
+		productDto.setPrice(product.getPrice());
+		productDto.setStock(product.getStock());
+		productDto.setImageBase64(product.getImageBase64());
+		productDto.setImageType(product.getImageType());
+		
+		return productDto;
+	}
+	
 	
 	
 }
