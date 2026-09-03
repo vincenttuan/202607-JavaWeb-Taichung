d84eb563d2ca47ce69a91e87ac586494b45c779b
FP <FP@FP>
2026-09-03 20:03:05 +0800

    web

---
+++ MemberWeb/src/main/java/controller/OrderController.java
@@ -0,0 +1,22 @@
+package controller;
+
+/**
+ * 訂購商品
+ * 
+ * 會依照 action 的參數內容有不同的行為
+ * -- GET ----
+ * action=         -> 訂購商品主頁
+ * action=cart     -> 查看購物車
+ * action=checkout -> 結帳頁
+ * action=history  -> 歷史訂單頁
+ * action=success  -> 交易成功頁
+ * 
+ * -- POST ----
+ * action=insert   -> 將商品新增到購物車
+ * action=update   -> 修改購物車商品
+ * action=remove   -> 移除購物車商品
+ * action=checkout -> 結帳
+ * */
+public class OrderController {
+
+}
