84cb358d12fb6af579b2b12ecb0a84a8479c5ea3
FP <FP@FP>
2026-09-17 19:26:23 +0800

    web

--- MemberWeb/src/main/java/model/dao/OrderDao.java
+++ MemberWeb/src/main/java/model/dao/OrderDao.java
@@ -54,30 +54,7 @@ public class OrderDao {
 	public List<Map<String, Object>> findAllOrders() {
 		List<Map<String, Object>> rows = new ArrayList<>();
 		
-		String sql = """
-				select
-				    o.id as order_id,
-				    m.id as member_id,
-				    m.username as member_name,
-				    m.email as member_email,
-				    o.total_amount,
-				    o.create_at,
-				    i.id as item_id,
-				    i.product_name,
-				    i.unit_price,
-				    i.quantity,
-				    i.subtotal
-				from member_order o
-				left join member m
-				    on m.id = o.member_id
-				left join order_item i
-				    on i.order_id = o.id
-				where i.id is not null
-				order by
-				    o.create_at desc,
-				    o.id desc,
-				    i.id;
-				""";
+		String sql = "SELECT * FROM find_all_ordes_view";
 		
 		try(Connection conn = DBUtil.getConnection();
 			Statement stmt = conn.createStatement();
